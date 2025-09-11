d9a21008134a8b9da4ad8ef2873e0ceb2690791b
mohidHussain <mohidhussaindot@gmail.com>
2025-09-12 01:14:34 +0500

    Update gsap.client.ts

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -1,14 +1,22 @@
-import { defineNuxtPlugin } from '#app'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
+// plugins/gsap.client.ts
+import { defineNuxtPlugin } from "#app";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-export default defineNuxtPlugin(() => {
-  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
+export default defineNuxtPlugin((nuxtApp) => {
+  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
-  return {
-    provide: {
-      gsap
-    }
-  }
-})
+  // patch gsap methods so they only run on client
+  const safeGsap = new Proxy(gsap, {
+    get(target, prop) {
+      if (process.server) {
+        // return a no-op function during SSR
+        return () => {};
+      }
+      return Reflect.get(target, prop);
+    },
+  });
+
+  nuxtApp.provide("gsap", safeGsap);
+});
