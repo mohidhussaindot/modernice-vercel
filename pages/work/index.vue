54f500490ea68447fa3fc4f28778578b7f97ab12
Hamza <deadshot6198@gmail.com>
2025-10-31 19:15:09 +0500

    made changes and added new routes /work-demo and changed /work

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <Work class="hidden md:block" />
-    <Projects class="block md:hidden" />
+    <Project2 />
+    <ScrollToTopButton />
+
+      <Navbar
+        :nav-links="[
+          { label: 'modernice.design', to: '/' },
+          { label: 'Services', to: '' },
+          { label: 'Work', to: '/work' }
+        ]"
+        cta-label="Let’s Talk"
+        cta-to="/appointment"
+        logo-src="/first-three-sect-img/navbar-img.png"
+        cta-from="#01A3FF"
+        cta-to-color="#25CDDA"
+      />
+       <SiteFooter
+          bg-color="#020111"
+          textcolor="#FCD265"
+          quote="A goal without a plan is just a wish"
+          author="– Antoine de Saint-Exupéry"
+        />
   </div>
 </template>
 
 <script setup>
-  import Projects from './projects.vue'
-  import Work from '@organisms/Work.vue'
+import ScrollToTopButton from '@organisms/Scrolltotop.vue';
+ import Project2 from '@organisms/project2.vue';
+ import Navbar from '@organisms/Navbar.vue';
+ import SiteFooter from '@organisms/SiteFooter.vue';
 </script>
